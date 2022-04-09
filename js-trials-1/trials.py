4ef94a15131b2c51f1da88d5985600b60dfa6981
akozlova <annakozlova92@gmail.com>
2022-04-09 19:12:01 +0000

    add project for js ex1

--- js-trials-1/trials.py
+++ js-trials-1/trials.py
@@ -2,24 +2,50 @@
 
 
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
+    """ print all items in a given list """
+    res = [print(item) for item in items]
+    return res
 
+# output_all_items([1, 'hello', True])
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
+    """ print all even items in a given list of nums """
+    res = [print(num) for num in nums if num % 2 == 0]
+    return res
 
+# get_all_evens([1, 30, 10, 300])
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
+    """ Given an array, return all elements at odd numbered indices. """
+    res = [print(item) for index, item in enumerate(items) if index % 2 != 0]
+    return res
 
+# get_odd_indices([1, 30, 10, 300, 344, 677])
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+    """ Given an array, output a numbered list. """
+    i = 1
+    for item in items:
+        print(f'${i}. ${item}')
+        i += 1
 
+# print_as_numbered_list([1, 30, 10, 300, 344, 677])
+# function getRange(start, stop) {
+#   const nums = [];
 
+#   for (let num = start; num < stop; num += 1) {
+#     nums.push(num);
+#   }
+#   return nums;
+# }
 def get_range(start, stop):
-    pass  # TODO: replace this line with your code
+    nums = []
+    num = start
 
+    for (i in some_list):
+        nums.append(i)
+
+get_range()
 
 def censor_vowels(word):
     pass  # TODO: replace this line with your code
