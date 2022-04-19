a59133d8e1e7b1d7c66afc76e98a5b19bfdda551
akozlova <annakozlova92@gmail.com>
2022-04-19 20:17:47 +0000

    finish with furtherStudy exercises with Sets

--- js-trials-2/furtherStudy.js
+++ js-trials-2/furtherStudy.js
@@ -1,8 +1,29 @@
 'use strict';
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  // Return a list of unique words that appear in `words1` and `words2`
+
+  // Arguments:
+  //     words1 (list): A list of words
+  //     words2 (list): Another list of words
+
+  // Returns:
+  //     list: A list of unique words
+
+  const firstString = new Set(words1.split(' '))
+  const secondString = new Set(words2.split(' '))
+  const result = new Set();
+  
+  for (let word1 of firstString) {
+    if (secondString.has(word1)) {
+      result.add(word1)
+    }
+  }
+
+  console.log('result',result)
 }
+
+wordsInCommon('JavaScript allows you you you to index into string and arrays arrays have', 'you will have to know how to get the length of a string string')
 
 function kidsGame(names) {
   // Replace this with your code
